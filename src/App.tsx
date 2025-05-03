9a7ab9b923525a915f13f048592622628d1b8ff9
Rasreal <ersultan.t16@gmail.com>
2025-05-03 14:53:09 +0500

    feat: About page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import React, { useState, useCallback } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/HomePage/HomePage";
+import Home from "@/pages/HomePage/HomePage";
+import About from "@/pages/AboutPage/AboutPage";
 
-
-
-const About = () => (
-  <div style={{ padding: "2rem" }}>
-    <h1>About This App</h1>
-    <p>This is a drawing app built with React and React Router.</p>
-  </div>
-);
 
 const App = () => (
   <BrowserRouter>
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,8 @@
+const About = () => (
+    <div style={{ padding: "2rem" }}>
+      <h1>About This App</h1>
+      <p>This is a drawing app built with React and React Router.</p>
+    </div>
+  );
+
+export default About;
